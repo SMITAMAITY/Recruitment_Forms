731db18051a03569b51947a0396f77679de70550
SMITAMAITY <smitam996@gmail.com>
2020-04-10 19:36:38 +0530

    first commit

---
+++ RecruitmentForms/src/main/java/com/lti/RecruitmentForms/RecruitmentFormsApplication.java
@@ -0,0 +1,13 @@
+package com.lti.RecruitmentForms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentFormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentFormsApplication.class, args);
+	}
+
+}
---
+++ RecruitmentForms/src/test/java/com/lti/RecruitmentForms/RecruitmentFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.RecruitmentForms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentFormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
